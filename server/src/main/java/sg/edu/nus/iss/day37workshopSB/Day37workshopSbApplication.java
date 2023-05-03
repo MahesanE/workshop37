f05296d46668d0fa764ce242b28a88f55479e57c
your_name <121241559+MahesanE@users.noreply.github.com>
2023-05-03 14:42:49 +0800

    first commit

---
+++ server/src/main/java/sg/edu/nus/iss/day37workshopSB/Day37workshopSbApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day37workshopSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37workshopSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37workshopSbApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/sg/edu/nus/iss/day37workshopSB/Day37workshopSbApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day37workshopSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37workshopSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
